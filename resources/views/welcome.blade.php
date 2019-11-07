6e928c0125986732d7081d8d4391981776375402
Andres Posada <andres.posada0919@gmail.com>
2019-11-06 22:19:59 -0500

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Manage Companies
                 </div>
 
                 <div class="links">
