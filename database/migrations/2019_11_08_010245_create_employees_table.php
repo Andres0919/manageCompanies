7942fcfaf4c207c680f1c3f756c9e67786d5bf10
Andres Posada <andres.posada0919@gmail.com>
2019-11-07 20:44:59 -0500

    migration and user seeder

---
+++ database/migrations/2019_11_08_010245_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('FirstName');
+            $table->string('LastName');
+            $table->integer('Company_id')->unsigned()->index();
+            $table->foreign('Company_id')->references('id')->on('companies');
+            $table->string('Email')->nullable();
+            $table->string('Phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
