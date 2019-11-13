1029d3fafea60043a819bc502d2b0725c7e47b02
Andres Posada <andres.posada0919@gmail.com>
2019-11-12 20:49:33 -0500

    locale middleware

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::middleware('locate')->group(function() {
+Route::middleware(['locate'])->group(function() {
 
 
     Route::get('/', function () {
@@ -19,26 +19,41 @@ Route::middleware('locate')->group(function() {
 
     Auth::routes();
 
-    Route::get('/home', 'HomeController@index')->name('home');
-
-    Route::resource('companies', 'CompanyController');
-    Route::resource('employees', 'EmployeeController');
-
-    Route::get('/console', function () {
-        Artisan::call('migrate:refresh', [
-            '--force' => true,
-        ]);
-
-        Artisan::call('db:seed');
-
-        Artisan::call('config:cache');
-
-        return redirect('/');
-    });
-
-    Route::get('/lang/{locale}', function ($locale){
-        Session::put('locale', $locale);
-
-        return back();
-    });
+    Route::middleware(['auth'])->group(function(){
+        Route::get('/home', 'HomeController@index')->name('home');
+    
+        Route::resource('companies', 'CompanyController');
+        Route::resource('employees', 'EmployeeController');
+    
+        Route::get('/console', function () {
+        
+            Artisan::call('config:cache');
+            
+            
+            Artisan::call('migrate:refresh', [
+                '--force' => true,
+            ]);
+            Artisan::call('db:seed');
+    
+            Artisan::call('config:cache');
+            
+    
+            return redirect('/');
+        });
+        
+        Route::get('/cache', function () {
+    
+            Artisan::call('config:cache');
+            Artisan::call('cache:clear');
+    
+            return redirect('/');
+        });
+        
+    
+        Route::get('/lang/{locale}', function ($locale){
+            Session::put('locale', $locale);
+    
+            return back();
+        });
+    });    
 });
